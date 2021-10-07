71e50f2724b697c6507749c98579eeadf0b65e30
Piumir3 <piumi.18@itfac.mrt.ac.lk>
2021-10-07 20:09:30 +0530

    Add book category

--- LibraryManagementSystem/routes/web.php
+++ LibraryManagementSystem/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +10,56 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+ 
+// Unauthenticated group 
+ 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+		 
+
+		 
+ 	
+
+	 
+
+	 
+ 
+    
+    // Render search books panel
+    Route::get('/book', array(
+        'as' => 'search-book',
+        'uses' => 'BooksController@searchBook'
+    ));    
+	
+
+
+// Main books Controlller left public so that it could be used without logging in too
+Route::resource('/books', 'BooksController');
+
+ 
+
+	// Render Add Books panel
+    Route::get('/add-books', array(
+        'as' => 'add-books',
+        'uses' => 'BooksController@renderAddBooks'
+	));
+
+	Route::get('/add-book-category', array(
+        'as' => 'add-book-category',
+        'uses' => 'BooksController@renderAddBookCategory'
+	));
+	
+	Route::post('/bookcategory', 'BooksController@BookCategoryStore')->name('bookcategory.store');
+	
+
+	// Render All Books panel
+    Route::get('/all-books', array(
+        'as' => 'all-books',
+        'uses' => 'BooksController@renderAllBooks'
+	));
+	
+	Route::get('/bookBycategory/{cat_id}', array(
+        'as' => 'bookBycategory',
+        'uses' => 'BooksController@BookByCategory'
+    ));
+
+	 
